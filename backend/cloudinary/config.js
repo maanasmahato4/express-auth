e3b47ae5e025e5a4e4d5624b3921b7daf87c4c51
maanasmahato4 <maanasmahato456@gmail.com>
2023-11-03 01:00:14 +0545

    fixed: env variables

--- backend/cloudinary/config.js
+++ backend/cloudinary/config.js
@@ -1,16 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
-/* cloudinary.config({
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
-}); */
-
-cloudinary.config({
-    cloud_name: 'dqn88yr6y', 
-    api_key: '825141699117774', 
-    api_secret: 'XlrXxTRQSHisQKwVeUyKexg-SFI' 
 });
-  
 
 module.exports = {cloudinary};
